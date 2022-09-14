9bd3a740dadd645a34c40aba093f03f7b8aad102
opeyemi akinla <opeyemiakinla1@gmail.com>
2022-09-14 16:24:44 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the lowercase alphabet 10 times on the new lines
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char c 'a';
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 26; j++)
+		{
+			_putchar(c + J);
+		}
+		_putchar(10);
+	}
+}
