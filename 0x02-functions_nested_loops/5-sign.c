a0c6c7a415da79def06b518538cf108aadd0f9c4
opeyemi akinla <opeyemiakinla1@gmail.com>
2022-09-14 17:21:13 -0700

    Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,4 @@
-#inlude "main.h"
+#include "main.h"
 
 /**
  * print_sign - prints the sign of a number 
@@ -13,7 +13,11 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	else if (n < o)
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	else 
 	{
 		_putchar('0');
 		return (0);
