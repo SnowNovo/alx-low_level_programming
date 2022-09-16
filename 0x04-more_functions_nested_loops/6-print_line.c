4a22b7ad68ef38d87deea98e201ac8bf1524f9e3
opeyemi akinla <opeyemiakinla1@gmail.com>
2022-09-15 19:47:49 -0700

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - prints line using "_" character of a n length
+ * @n: length of the line
+ *
+ * Return: void
+ */
+void print-line(int n)
+{
+	int i = 0;
+
+	if (n > 0)
+	{
+		while (i < n)
+		{
+			_putchar(95);
+			i++;
+		}
+	}
+	_putchar('\n');
+}
