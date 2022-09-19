5b270f9250f7fbca7bd2065a87155e2b16c6f2b3
opeyemi akinla <opeyemiakinla1@gmail.com>
2022-09-19 14:15:30 -0700

    TASK 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,7 +1,15 @@
 #include "main.h"
 
 /**
- * swap_int - swaps the values ofpointed at the integers
+ * swap_int - swaps the values pointed to by arguements
  *
  * @a: pointer arguement 1
- * @b: pointer arguement 2 
+ * @b: pointer arguement 2
+ */
+void swap_int(int *a, int*b)
+{
+	int con = *a;
+
+	*a = *b;
+	*b = con;
+}
