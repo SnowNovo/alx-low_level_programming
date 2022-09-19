44bb8be95f45ea5db2dfb17e2dd4cda276bdaa6c
opeyemi akinla <opeyemiakinla1@gmail.com>
2022-09-19 14:48:52 -0700

    TASK 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a given string
+ * @s: passed pointer argument for string
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int len = 0, length;
+	char c;
+
+	while (*(s + len) != '\0')
+	{
+		len++;
+	}
+	length = len;
+	len--;
+	for (i = 0; i < length / 2; i++)
+	{
+		c = *(s + i);
+		*(s + i) = *(s + len);
+		*(s + len) = c;
+		len--;
+	}
+}
