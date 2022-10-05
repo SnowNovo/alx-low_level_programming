dbd2721aa132990ed3ee44452337d9ea405fd67f
opeyemi akinla <opeyemiakinla1@gmail.com>
2022-10-04 19:12:27 -0700

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * *create_array - creates an array of chars, and
@@ -24,4 +25,3 @@ char *create_array(unsigned int size, char c)
 		array[index] = c;
 
 	return (array);
-}
